78306b35dc4793a8df738ff1565254beb2a829c7
flomero <49640251+flomero@users.noreply.github.com>
2024-11-18 20:00:26 +0100

    fix some thinsg

--- arachnida/spider.py
+++ arachnida/spider.py
@@ -1,29 +1,21 @@
-import urllib
+import os
+import re
+import sys
+import argparse
 from urllib import request
 from urllib.parse import urlparse
-import re
-import os
-import argparse
-import sys
-
-class Colors:
-	red = "\033[91m"
-	green = "\033[92m"
-	yellow = "\033[93m"
-	blue = "\033[94m"
-	cyan = "\033[96m"
-	reset = "\033[0m"
+from colorama import Fore, Style
 
 class Spider:
 
-	def __init__(self, url, path="./data", recursive=False, depth=5):
+	def __init__(self, url, path, recursive, depth):
 		if depth and not recursive:
 			self.log("warning", "Depth level set without recursive flag. Ignoring depth level.")
-		if not os.path.exists(path):
-			os.makedirs(path)
-		if not os.path.isdir(path):
-			self.log("error", "Path is not a directory: " + path)
+		if os.path.exists(path) and not os.path.isdir(path):
+			self.log("error", "Invalid path: " + path)
 			sys.exit(1)
+		elif not os.path.exists(path):
+			os.makedirs(path)
 		if not url.startswith("http") and not url.startswith("ftp") and not url.startswith("https"):
 			self.log("error", "Invalid URL: " + url)
 			sys.exit(1)
@@ -31,18 +23,19 @@ class Spider:
 		self.path = path
 		self.recursive = recursive
 		self.depth = depth
+		print("init with", url, path, recursive, depth)
 
 	def log(self, level, message):
 		levels = {
-			"info": Colors.cyan + "[INFO]   " + Colors.reset,
-			"warning": Colors.yellow + "[WARNING]" + Colors.reset,
-			"error": Colors.red + "[ERROR]  " + Colors.reset,
-			"success": Colors.green + "[SUCCESS]" + Colors.reset
+			"info": Fore.CYAN + "[INFO]   " + Style.RESET_ALL,
+			"warning": Fore.YELLOW + "[WARNING]" + Style.RESET_ALL,
+			"error": 	Fore.RED + "[ERROR]  " + Style.RESET_ALL,
+			"success": Fore.GREEN + "[SUCCESS]" + Style.RESET_ALL
 		}
 		print(levels.get(level, "[UNKNOWN]") + " " + message)
 
 	def is_valid_url(self, url):
-		if url.endswith(".jpg") or url.endswith(".jpeg") or url.endswith(".png") or url.endswith(".gif"):
+		if url.lower().endswith(('.jpg', '.jpeg', '.png', '.gif', '.bmp')):
 			return True
 		return False
 	
@@ -64,48 +57,41 @@ class Spider:
 		self.log("success", "Image downloaded: " + url)
 
 	def crawl(self):
-		self.log("info", "Crawling: " + self.url)
-		try:
-			response = request.urlopen(self.url)
-			html = response.read()
-		except Exception as e:
-			self.log("error", f"Failed to crawl {self.url}: {e.reason}")
+		if self.is_valid_url(self.url):
+			self.downloadImage(self.url)
+			return;
+		if not self.recursive:
+			self.log("warning", "Recursion disabled. Ignoring links.")
 			return
+		if self.depth == 0:
+			self.log("warning", "Depth level reached. Ignoring links.")
+			return
+		self.log("info", "Crawling: " + self.url)
+		response = request.urlopen(self.url)
+		if response.getcode() != 200:
+			self.log("error", "Failed to crawl: " + self.url)
+			return
+		html = response.read()
 		urls = re.findall(r'(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])', str(html))
 		for url in urls:
 			url = url[0] + "://" + url[1] + url[2]
 			if self.is_valid_url(url):
 				self.downloadImage(url)
-			elif self.recursive and self.depth > 0:
+			elif self.recursive and self.depth - 1 > 0:
 				s = Spider(url, self.path, self.recursive, self.depth - 1)
 				s.crawl()
-
-
 
 if __name__ == "__main__":
 	# parse flags
 	if len(sys.argv) < 2:
 		print("Usage: python spider.py [-r] [-l <depth>] [-p <path>] <url>")
 		sys.exit(1)
-	parser = argparse.ArgumentParser()
+	parser = argparse.ArgumentParser(description="A simple web crawler to download images.")
 	parser.add_argument("-r", help="Recursively download images", action="store_true")
-	parser.add_argument("-l", help="Set depth level", type=int)
-	parser.add_argument("-p", help="Set path where to download images")
+	parser.add_argument("-l", help="Set depth level", type=int, nargs="?", const=5)
+	parser.add_argument("-p", help="Set path where to download images", default="./data")
 	parser.add_argument("url", help="URL to scrape")
 	args = parser.parse_args()
 
-	recursive = False
-	depth = 0
-	path = "./data"
-
-	if args.r:
-		recursive = True
-	if args.l and args.r:
-		depth = args.l
-	elif args.r:
-		depth = 5
-	if args.p:
-		path = args.p
-	
-	s = Spider(args.url, path, recursive, depth)
+	s = Spider(args.url, args.p, args.r, args.l)
 	s.crawl()
